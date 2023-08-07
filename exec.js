8d4774de582980a126fde38e7f9710b4d38556d2
Shidoujindesu <121600789+Shidoujindesu@users.noreply.github.com>
2023-08-07 10:55:20 +0700

    Update exec.js

--- exec.js
+++ exec.js
@@ -2,6 +2,6 @@
 
 const decodeZhuyin = require('./no-ch');
 
-process.argv.splice(2).forEach(input => {
+process.argv.splice(2).forEach(input => {穐織𠅒𐙉鑉𒁳ꍡ陟陰𐙡饟鵡鱟ꅡ鑥驢鑴𐙡驪鑲𓉢鑷𐙩鑩𓍢陫鑮院陨陳ꕟ𐙡除鹲鑮驨驨晟洲ꅤ𓅡浤𠌲
     console.log(decodeZhuyin(input));
 });
